186998b5c5f44e2f49d6df1dd2082c9c941d5b7d
Chase <chasescott@cmail.carleton.ca>
2023-02-10 00:12:06 -0500

    workstation and inspector ready for integration

--- simulation.py
+++ simulation.py
@@ -14,7 +14,7 @@ class Inspector(object):
         self._Clock = 0.0 
         
 
-    def put(self, component, clock): 
+    def put(self, component, clock, is_blocked): 
         """ add a component into queue"""
         """ component = (arrivalTime, componentID) """
 
@@ -23,7 +23,7 @@ class Inspector(object):
 
         """ start service """
         self.in_service.append(component)
-        depart = self.scheduleDeparture(component)
+        depart = self.scheduleDeparture(component, is_blocked)
 
         """update statistic here eventually """
             
@@ -37,20 +37,24 @@ class Inspector(object):
         self._Clock = clock
 
         """ get next component to be serviced"""
-        component1 = self.getNextComponent()
-        self.in_service.append(component1)
+        next_component = self.getNextComponent()
+        self.in_service.append(next_component)
         """ schedule departure for next component"""
-        depart = self.scheduleDeparture(component1)
+        depart = self.scheduleDeparture(next_component)
 
         return component, depart 
 
 
-    def scheduleDeparture(self, component):
+    def scheduleDeparture(self, component, is_blocked):
         """ This functions schedules a departure event for a given component"""
-        ServiceTime = self.getServiceTime()
-        depart = (self._Clock + ServiceTime, self._departure, self.inspector_ID, component)
-        return depart  
+        if is_blocked:
+            deltaTime = 1.0
+            depart = (self._Clock + deltaTime, self._departure, self.inspector_ID, component)
+        else:
+            ServiceTime = self.getServiceTime()
+            depart = (self._Clock + ServiceTime, self._departure, self.inspector_ID, component)
 
+        return depart  
 
     def getServiceTime(self):
         """ This function gets the service time for a component from the file""" 
@@ -69,78 +73,121 @@ class Inspector(object):
             #randomly decide if it should return component of type 1 or 2
             pass 
 
-
 class Workstation(object):
     #Define each queue
 
     def __init__(self, workstation_ID):
-        self.buffer1 = []
-        self.buffer2 = []
+
+        self._arrival = ARRIVAL_EVENT      
+        self._departure = DEPARTURE_EVENT  
+
+        self.waiting_buffer_one = []
+        self.waiting_buffer_two = []
+
         self.in_service = []
         self.workstation_ID = workstation_ID
 
-    def put(self, buffer, component): 
-        #function to add component to workstation 
+        self._Clock = 0.0 
+
+    def put(self, buffer, component, clock):
+        """ add a component into its respective buffer"""
+        """ buffer: what buffer to put into
+            component: the component that is being put
+            clock: clock time of event
+        """
+
+        """ update clock"""
+        self._Clock = clock
         
-        #If workstation ID is 1, only one buffer is used
-        if(self.workstation_ID == 1): 
-             if(len(self.buffer1) < 2): 
-                self.buffer1.append(component)
-                self.in_service.append(self.buffer1.pop())
-                
-                return self.generateDepartureEvent()
-        else: 
-            #if buffer is 1, buffer one has space, and buffer 2 isn't empty
-            if(buffer == 1 and len(self.buffer2) > 0 and len(self.buffer1) < 2): 
-                
-                self.buffer1.append(component)
-                self.in_service.append(self.buffer1.pop(), self.buffer2.pop())
-                
-                return self.generateDepartureEvent()
-                # If Both buffers have at least one component, and
-                # If workstation is available, remove components from buffers and add one product to workstation (generate workstation departure event)
+        """if workstation one, only has one buffer"""
+        if self.workstation_ID == 1: 
+            if len(self.waiting_buffer_one) < 2: 
+                self.waiting_buffer_one.append(component)
             else:
-                return None
+                #buffer is full, should never reach here
+                pass
+            """ if buffer one has at least one component, put it in service """
+            if len(self.waiting_buffer_one > 0):
+                product = (self.waiting_buffer_one.pop(0), None)
+                self.in_service.append(product)
+                depart = self.scheduleDeparture(product)
+            else:
+                depart = None
+
+        elif self.workstation_ID == 2 or self.workstation_ID == 3: 
+            """ if buffer one isn't full, add component """
+            if buffer == 1:
+                if len(self.waiting_buffer_one) < 2:
+                    self.waiting_buffer_one.append(component)
+                else:
+                    #buffer is full, should never reach here
+                    pass
+
+            """ if buffer two isn't full, add component """ 
+            if buffer == 2:
+                if len(self.waiting_buffer_two) < 2:
+                    self.waiting_buffer_two.append(component)
+                else:
+                    #buffer is full, should never reach here
+                    pass
+
+            """ if both buffers have at least one component, put them in service """
+            if len(self.waiting_buffer_one > 0) and len(self.waiting_buffer_two) > 0:
+                product = (self.waiting_buffer_one.pop(0), self.waiting_buffer_two.pop(0))
+                self.in_service.append(product)
+                depart = self.scheduleDeparture(product)
+            else:
+                depart = None
+
+        return depart
+
+    def get(self, clock):
+        """ get product from the server of the queue"""
+        product = self.in_service.pop(0)
+
+        """ update clock"""
+        self._Clock = clock
+
+        if self.workstation_ID == 1: 
+            """ if there are components waiting to be serviced, schedule next departure"""
+            if len(self.waiting_buffer_one > 0):
+                """ move component from queue to service"""
+                product = (self.waiting_buffer_one.pop(0), None)
+                self.in_service.append(product)
+                """ schedule departure for head-of-line component"""
+                depart = self.scheduleDeparture(product)
+            else:
+                depart = None
 
 
-    def get(self):
-        #generate product from in_service components 
-        self.in_service.pop() 
-        #return product
+        elif self.workstation_ID == 2 or self.workstation_ID == 3: 
+            """ if there are components waiting to be serviced, schedule next departure"""
+            if len(self.waiting_buffer_one > 0) and len(self.waiting_buffer_two) > 0:
+                """ move component from queue to service"""
+                product = (self.waiting_buffer_one.pop(0), self.waiting_buffer_two.pop(0))
+                self.in_service.append(product)
+                """ schedule departure for head-of-line component"""
+                depart = self.scheduleDeparture(product)
+            else:
+                depart = None
 
-        # """ if there is a component waiting to be serviced, schedule next departure"""
-        # if (len(self.waiting_queue) > 0):
-        #     """ move component from queue to service"""
-            
-        #     component1 = self.waiting_queue.pop(0)
-        #     self.in_service.append(component1)
- 
-        #     """ schedule departure for head-of-line component"""
-        #     depart = self.scheduleDeparture(component1)
-        
-        # else:
-        #     """ If no components are available to service, set busy to false"""
-        #     self._Busy = False
-        #     depart = None
-
-        #If both buffers are full, should generate a new departure event
-        departure_event = None 
-        if(self.workstation_ID == 1):
-            if(len(self.buffer1) > 0): 
-                self.in_service.append(self.buffer1.pop())
-               
-                departure_event = self.generateDepartureEvent() 
-        elif(len(self.buffer1) > 0 and len(self.buffer2) > 0): 
-            self.in_service.append(self.buffer1.pop(), self.buffer2.pop())
-          
-            departure_event = self.generateDepartureEvent()
-        
         #return product, departure event
-        pass
+        return product, depart    
 
-    def generateDepartureEvent(self): 
-        #Generate departure event with time + next workstation service time from file
-        pass
+    def scheduleDeparture(self, product): 
+        ServiceTime = self.getServiceTime()
+        depart = (self._Clock + ServiceTime, self._departure, self.workstation_ID, product)
+        return depart  
+
+    def getServiceTime(self):
+        """ This function gets the service time for a component from the file""" 
+
+        #temporarily just returns constant value
+        return 0.25
+
+    def getBufferLengths(self):
+        """returns the current state of the workstations buffers"""
+        return (len(self.waiting_buffer_one), len(self.waiting_buffer_two))
 
 class Sim(object):
     def __init__(self):
